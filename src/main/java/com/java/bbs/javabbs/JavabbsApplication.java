ddfdcdb7a6eb3b3d50ad94a628951f69bb8ab98d
unknown <1551282386@qq.com>
2020-04-01 16:54:40 +0800

    Initial commit

---
+++ src/main/java/com/java/bbs/javabbs/JavabbsApplication.java
@@ -0,0 +1,13 @@
+package com.java.bbs.javabbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavabbsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavabbsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/java/bbs/javabbs/JavabbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.bbs.javabbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavabbsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
